cae0b7edca4f72240f6c83eb3e1560a8e958a719
SC0RP1 <islam4136@bk.ru>
2026-05-20 22:04:33 +0400

    Create class Book

---
+++ models.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+class Book:
+    author: str
+    title: str
+    rating: int
+    date_read: str
+
+    def to_dict(self):
+        return {
+            "author": self.author,
+            "title": self.title,
+            "rating": self.rating,
+            "date_read": self.date_read
+        }
+
+    @classmethod
+    def from_dict(cls, data: dict):
+        return cls(
+            author=data['author'],
+            title=data['title'],
+            rating=data['rating'],
+            date_read=data['data_read']
+        )
+
+    def __str__(self):
+        return f"{self.author} - {self.title} (Оценка: {self.rating}, Дата: {self.date_read})"
